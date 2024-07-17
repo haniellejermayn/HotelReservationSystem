d0a53a04356c2bcb00e041636647372ff6705b9e
haniellejermayn <chua.haniellejermayn@gmail.com>
2024-07-17 14:26:04 +0800

    Added HRS MVC

--- GUI/Main.java
+++ GUI/Main.java
@@ -1,9 +1,9 @@
 import java.util.*;
 
+
 public class Main {
 
     public static void main(String[] args) {
-        
         // change to Hotel
         ArrayList<String> hotels = new ArrayList<String>();
 
---
+++ hrs/HRSModel.java
@@ -0,0 +1,41 @@
+package hrs;
+
+import java.util.ArrayList;
+
+public class HRSModel {
+    private ArrayList<Hotel> hotels;
+
+    public HRSModel() {
+        this.hotels = new ArrayList<>();
+    }
+
+    public void addHotel(Hotel hotel) {
+        hotels.add(hotel);
+    }
+
+    // Edit: fetchHotel
+    public Hotel getHotel(int index) {
+        return hotels.get(index);
+    }
+
+    public void removeHotel(int index) {
+        hotels.remove(index);
+    }
+
+    public int countHotels() {
+        return hotels.size();
+    }
+
+    public boolean isHotelNameUnique(String name) {
+        for (Hotel hotel : hotels) {
+            if (hotel.getHotelName().equals(name)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public ArrayList<Hotel> getHotels() {
+        return hotels;
+    }
+}
