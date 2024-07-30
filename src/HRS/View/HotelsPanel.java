d4423a460c3e6c3945f5446f430839ae78dcb4e6
haniellejermayn <chua.haniellejermayn@gmail.com>
2024-07-30 17:02:32 +0800

    HotelsPanel and IconButtton Javadoc

--- src/HRS/View/HotelsPanel.java
+++ src/HRS/View/HotelsPanel.java
@@ -1,11 +1,15 @@
 package src.HRS.View;
 
-import javax.swing.*;
-import src.HRS.Model.Hotel;
 import java.awt.*;
 import java.util.*;
+import javax.swing.*;
+import src.HRS.Model.Hotel;
 
-public class HotelsPanel extends LayeredRoundPanel{
+/**
+ * The {@code HotelsPanel} class represents a panel that displays a list of hotels with options to filter and add new hotels.
+ * It extends {@link LayeredRoundPanel} and provides interactive components such as a title label, filter button, and a scrollable list of hotel options.
+ */
+public class HotelsPanel extends LayeredRoundPanel {
     
     private JLabel hotelTitle;
     private CreateHotelPanel createHotelPanel;
@@ -21,12 +25,18 @@ public class HotelsPanel extends LayeredRoundPanel{
     private ArrayList<Hotel> hotels;
     private int nHotels;
 
-    public HotelsPanel(ArrayList<Hotel> hotels, int nHotels){
-
+    /**
+     * Constructs a {@code HotelsPanel} with the specified list of hotels and the number of hotels.
+     * Initializes the panel's components, including the title, buttons, filter panel, and the scrollable container for hotel options.
+     *
+     * @param hotels the list of {@code Hotel} objects to display
+     * @param nHotels the number of hotels to display
+     */
+    public HotelsPanel(ArrayList<Hotel> hotels, int nHotels) {
         super(new Color(13, 22, 45));
 
         this.hotels = hotels;
-        this. nHotels = nHotels;
+        this.nHotels = nHotels;
 
         customFont35 = Customization.createCustomFont("Fonts/POPPINS-SEMIBOLD.TTF", 35);
 
@@ -40,9 +50,9 @@ public class HotelsPanel extends LayeredRoundPanel{
 
         // * Hotel Catalogue * //
 
-        hotelCatalogue = new ArrayList<HotelOption>();
+        hotelCatalogue = new ArrayList<>();
 
-        for(int i = 0; i < nHotels; i++){
+        for (int i = 0; i < nHotels; i++) {
             HotelOption optionTemp = new HotelOption(hotels.get(i));
             initializeHotelOption(optionTemp, hotels.get(i), i);
             hotelCatalogue.add(optionTemp);
@@ -72,10 +82,10 @@ public class HotelsPanel extends LayeredRoundPanel{
         hotelContainerHeight = (nHotels + 1) * 10 + (nHotels * 110) + 70;
 
         hotelContainer = new RoundPanel(new Color(13, 22, 45));
-        hotelContainer.setLayout(null);;
+        hotelContainer.setLayout(null);
         hotelContainer.setPreferredSize(new Dimension(620, hotelContainerHeight));
 
-        for (int i = 0; i < nHotels; i++){
+        for (int i = 0; i < nHotels; i++) {
             hotelContainer.add(hotelCatalogue.get(i));
         }
 
@@ -93,8 +103,15 @@ public class HotelsPanel extends LayeredRoundPanel{
         this.add(hotelScrollPane, JLayeredPane.DEFAULT_LAYER);
     }
 
-    public void initializeHotelOption(HotelOption item, Hotel hotel, int itemNo){
-
+    /**
+     * Initializes the properties of a {@code HotelOption} item with the specified {@code Hotel} and item number.
+     * Sets the text for the hotel name, number of rooms, number of reservations, and price.
+     *
+     * @param item the {@code HotelOption} to initialize
+     * @param hotel the {@code Hotel} to set information for
+     * @param itemNo the index of the item in the list
+     */
+    public void initializeHotelOption(HotelOption item, Hotel hotel, int itemNo) {
         String hotelName = hotel.getHotelName(); 
 
         float price = hotel.getBasePrice(); 
@@ -110,95 +127,205 @@ public class HotelsPanel extends LayeredRoundPanel{
         item.getHotelRes().setText(String.valueOf(reservations) + " reservations");
         item.getPrice().setText("$" + String.format("%.2f", price));
         item.setVerticalAlignment(JLabel.CENTER);
-        
+
         item.setFocusable(false);
     }
 
-    public JLabel getHotelTitle(){
+    /**
+     * Returns the {@code JLabel} used as the title of the hotel panel.
+     *
+     * @return the hotel title label
+     */
+    public JLabel getHotelTitle() {
         return hotelTitle;
     }
 
-    public void setHotelTitle(JLabel hotelTitle){
+    /**
+     * Sets the {@code JLabel} used as the title of the hotel panel.
+     *
+     * @param hotelTitle the {@code JLabel} to set as the hotel title
+     */
+    public void setHotelTitle(JLabel hotelTitle) {
         this.hotelTitle = hotelTitle;
     }
 
-    public ArrayList<HotelOption> getHotelCatalogue(){
+    /**
+     * Returns the list of {@code HotelOption} objects representing the catalogue of hotels.
+     *
+     * @return the list of hotel options
+     */
+    public ArrayList<HotelOption> getHotelCatalogue() {
         return hotelCatalogue;
     }
 
-    public void setHotelCatalogue(ArrayList<HotelOption> hotelCatalogue){
+    /**
+     * Sets the list of {@code HotelOption} objects representing the catalogue of hotels.
+     *
+     * @param hotelCatalogue the list of hotel options to set
+     */
+    public void setHotelCatalogue(ArrayList<HotelOption> hotelCatalogue) {
         this.hotelCatalogue = hotelCatalogue;
     }
 
-    public IconButton getCreateHotelButton(){
+    /**
+     * Returns the {@code IconButton} used to create a new hotel.
+     *
+     * @return the create hotel button
+     */
+    public IconButton getCreateHotelButton() {
         return createHotelButton;
     }
 
-    public void setCreateHotelButton(IconButton createHotelButton){
+    /**
+     * Sets the {@code IconButton} used to create a new hotel.
+     *
+     * @param createHotelButton the {@code IconButton} to set
+     */
+    public void setCreateHotelButton(IconButton createHotelButton) {
         this.createHotelButton = createHotelButton;
     }
 
-    public CreateHotelPanel getCreateHotelPanel(){
+    /**
+     * Returns the {@code CreateHotelPanel} used for creating new hotels.
+     *
+     * @return the create hotel panel
+     */
+    public CreateHotelPanel getCreateHotelPanel() {
         return createHotelPanel;
     }
 
-    public void setCreateHotelPanel(CreateHotelPanel createHotelPanel){
+    /**
+     * Sets the {@code CreateHotelPanel} used for creating new hotels.
+     *
+     * @param createHotelPanel the {@code CreateHotelPanel} to set
+     */
+    public void setCreateHotelPanel(CreateHotelPanel createHotelPanel) {
         this.createHotelPanel = createHotelPanel;
     }
 
-    public IconButton getFilterButton(){
+    /**
+     * Returns the {@code IconButton} used to filter hotels.
+     *
+     * @return the filter button
+     */
+    public IconButton getFilterButton() {
         return filterButton;
     }
 
-    public void setFilterButton(IconButton filterButton){
+    /**
+     * Sets the {@code IconButton} used to filter hotels.
+     *
+     * @param filterButton the {@code IconButton} to set
+     */
+    public void setFilterButton(IconButton filterButton) {
         this.filterButton = filterButton;
     }
 
-    public FilterPanel getFilterPanel(){
+    /**
+     * Returns the {@code FilterPanel} used to filter the list of hotels.
+     *
+     * @return the filter panel
+     */
+    public FilterPanel getFilterPanel() {
         return filterPanel;
     }
 
-    public void setFilterPanel(FilterPanel filterPanel){
+    /**
+     * Sets the {@code FilterPanel} used to filter the list of hotels.
+     *
+     * @param filterPanel the {@code FilterPanel} to set
+     */
+    public void setFilterPanel(FilterPanel filterPanel) {
         this.filterPanel = filterPanel;
     }
 
-    public boolean getFilterPanelVisible(){
+    /**
+     * Returns whether the filter panel is currently visible.
+     *
+     * @return {@code true} if the filter panel is visible; {@code false} otherwise
+     */
+    public boolean getFilterPanelVisible() {
         return isVisible;
     }
 
-    public void setFilterPanelVisible(boolean isVisible){
+    /**
+     * Sets the visibility of the filter panel.
+     *
+     * @param isVisible {@code true} to make the filter panel visible; {@code false} to hide it
+     */
+    public void setFilterPanelVisible(boolean isVisible) {
         this.isVisible = isVisible;
     }
 
-    public ScrollPaneCustom getHotelScrollPane(){
+    /**
+     * Returns the {@code ScrollPaneCustom} used to scroll through the list of hotel options.
+     *
+     * @return the hotel scroll pane
+     */
+    public ScrollPaneCustom getHotelScrollPane() {
         return hotelScrollPane;
     }
 
-    public void setHotelScrollPane(ScrollPaneCustom hotelScrollPane){
+    /**
+     * Sets the {@code ScrollPaneCustom} used to scroll through the list of hotel options.
+     *
+     * @param hotelScrollPane the {@code ScrollPaneCustom} to set
+     */
+    public void setHotelScrollPane(ScrollPaneCustom hotelScrollPane) {
         this.hotelScrollPane = hotelScrollPane;
     }
 
-    public RoundPanel getHotelContainer(){
+    /**
+     * Returns the {@code RoundPanel} containing the hotel options.
+     *
+     * @return the hotel container panel
+     */
+    public RoundPanel getHotelContainer() {
         return hotelContainer;
     }
 
-    public void setHotelContainer(RoundPanel hotelContainer){
+    /**
+     * Sets the {@code RoundPanel} containing the hotel options.
+     *
+     * @param hotelContainer the {@code RoundPanel} to set
+     */
+    public void setHotelContainer(RoundPanel hotelContainer) {
         this.hotelContainer = hotelContainer;
     }
 
-    public ArrayList<Hotel> getHotels(){
+    /**
+     * Returns the list of {@code Hotel} objects.
+     *
+     * @return the list of hotels
+     */
+    public ArrayList<Hotel> getHotels() {
         return hotels;
     }
 
-    public void setHotels(ArrayList<Hotel> hotels){
-        this. hotels = hotels;
+    /**
+     * Sets the list of {@code Hotel} objects.
+     *
+     * @param hotels the list of {@code Hotel} objects to set
+     */
+    public void setHotels(ArrayList<Hotel> hotels) {
+        this.hotels = hotels;
     }
 
-    public int getnHotels(){
+    /**
+     * Returns the number of hotels currently displayed.
+     *
+     * @return the number of hotels
+     */
+    public int getnHotels() {
         return nHotels;
     }
 
-    public void setnHotels(int nHotels){
+    /**
+     * Sets the number of hotels to display.
+     *
+     * @param nHotels the number of hotels to set
+     */
+    public void setnHotels(int nHotels) {
         this.nHotels = nHotels;
     }
 }
