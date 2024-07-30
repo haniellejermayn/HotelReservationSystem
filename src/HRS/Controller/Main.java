425bf5f099de5bedfc418069d1bd472380cbee15
haniellejermayn <chua.haniellejermayn@gmail.com>
2024-07-30 16:34:53 +0800

    Main Javadoc

--- src/HRS/Controller/Main.java
+++ src/HRS/Controller/Main.java
@@ -4,8 +4,19 @@ package src.HRS.Controller;
 import src.HRS.Model.HRSModel;
 import src.HRS.View.HRSView;
 
+/**
+ * The main class for launching the Hotel Reservation System application.
+ * 
+ * <p>This class is the entry point of the application. It initializes the model,
+ * view, and controller, and starts the application.</p>
+ */
 public class Main {
 
+    /**
+     * The main method to launch the Hotel Reservation System application.
+     * 
+     * @param args Command-line arguments (not used in this application).
+     */
     public static void main(String[] args) {
 
         HRSModel model = new HRSModel();
